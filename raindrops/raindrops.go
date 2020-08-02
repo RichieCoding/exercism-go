61bd9c97062a4736eb145256e945bef039fdb8e3
Richard Li <richardli125@gmail.com>
2020-08-02 11:44:57 -0400

    added raindrop challenge

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,5 +1,27 @@
+// Package raindrops converts a number to a string depending on the numbers factors
 package raindrops
 
-func Raindrops(n int) string {
-	return 
+import "strconv"
+
+// Convert recieves an int and depending on the int will return Pling, Plang, Plong or the original int
+func Convert(n int) string {
+	var raindrop string
+
+	if n%3 == 0 {
+		raindrop += "Pling"
+	}
+
+	if n%5 == 0 {
+		raindrop += "Plang"
+	}
+
+	if n%7 == 0 {
+		raindrop += "Plong"
+	}
+
+	if raindrop != "" {
+		return raindrop
+	}
+
+	return strconv.Itoa(n)
 }

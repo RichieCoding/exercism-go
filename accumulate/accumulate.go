328eefa4916875ab655eea07a5029c9e18fda162
Richard Li <richardli125@gmail.com>
2021-01-10 14:57:25 -0500

    adjusted accumulate function

--- accumulate/accumulate.go
+++ accumulate/accumulate.go
@@ -1,20 +1,12 @@
 package accumulate
 
-import "strings"
-
 // Accumulate recieves a collection of strings and returns each string uppercased
-func Accumulate(s []string, upper func(string)string) []string {
+func Accumulate(s []string, upper func(string) string) []string {
 	var ns []string
 
-	if len(s) != 0 {
-		for _, word := range s {
-			ns = append(ns, upper(word))
-		}
+	for _, word := range s {
+		ns = append(ns, upper(word))
 	}
 
 	return ns
-}
-
-func toUppercase(s string) string {
-	return strings.ToUpper(s)
 }

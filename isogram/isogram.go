efb6cbcc7a9568aeece7a06c47461a9f2ded6bfd
Richard Li <richardli125@gmail.com>
2021-01-23 10:47:41 -0500

    refactoed isogram to not use ReplaceAll method

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,19 +1,18 @@
 package isogram
 
 import (
-	"strings"
 	"unicode"
 )
 
 // IsIsogram determines if a string contains any repeating letters, if so it is considered an isogram
 func IsIsogram(s string) bool {
-	s = strings.ReplaceAll(s, "-", "")
-	s = strings.ReplaceAll(s, " ", "")
-
 	var occurances = make(map[rune]bool)
 
 	for _, letter := range s {
 		letter = unicode.ToUpper(letter)
+		if letter == '-' || letter == ' ' {
+			continue
+		}
 		if occurances[letter] {
 			return false
 		}
